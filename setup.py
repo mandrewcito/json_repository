78f2678dd75592846f6e1271304456d7c0b729c4
mandrewcito <anbaalo@gmail.com>
2021-03-07 00:25:46 +0100

    feat(version): increment version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="json_repository",
-    version="0.2.1",
+    version="0.2.2",
     author="mandrewcito",
     author_email="anbaalo@gmail.com",
     description="A simple json repository",
